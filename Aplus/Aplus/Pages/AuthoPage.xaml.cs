ae0711fdcdccda8fa2a26436a3a1dcc20c9940c5
unknown <sematkinartem6@gmail.com>
2022-02-11 23:33:37 +0300

    first commit

---
+++ Aplus/Aplus/Pages/AuthoPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplus.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthoPage : ContentPage
+    {
+        public AuthoPage()
+        {
+            InitializeComponent();
+        }
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProjectsPage());
+        }
+
+        private async void Reg_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegPage());
+        }
+    }
+}
---
+++ Aplus/Aplus/Pages/RegPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplus.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegPage : ContentPage
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+        private async void RegistrationVerification_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
